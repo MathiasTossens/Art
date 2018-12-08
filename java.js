d3370d330e7699e735df908b87084590ebcfa651
jogjo <Mathias.tossens@Gmail.com>
2018-12-08 18:36:33 +0100

    Initial commit

---
+++ java.js
@@ -0,0 +1,18 @@
+ScrollReveal().reveal('.vignette', {
+    delay: 200,
+    rotate: {
+        x: 0,
+        z: 5
+        } 
+})
+ScrollReveal().reveal('.slideinleft', {
+    delay: 200,
+    origin: 'left',
+    distance: '200px'
+})
+ScrollReveal().reveal('#bigdown', {
+    delay: 1000,
+    origin: 'top',
+    distance: '100px',
+})
+$('#gallery').lightGallery();
